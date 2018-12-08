6d49e793d57059b8fa67b55210e12d403a3930ba
WillKoehrsen <wjk68@case.edu>
2018-12-08 11:29:38 -0500

    Added default_cell code

---
+++ default_cell/README.md
@@ -0,0 +1,16 @@
+Default Cell
+=========
+
+Adds and runs a default cell at the top of each new notebook. To change the default cell, edit the `main.js` file in the code:
+
+`Jupyter.notebook.insert_cell_above('code', 0).set_text(``)`
+
+Set the `text` to whatever you would like.
+
+This extension also adds a button to the toolbar allowing you to insert and run the
+default cell above your current cell. This can be helpful if you open an already started notebook
+and notice you are missing some common imports.
+
+This extension is a work in progress and any help would be appreciated. Feel free to make contributions
+on GitHub or contact the author (Will Koehrsen) at
+wjk68@case.edu
---
+++ default_cell/main.js
@@ -0,0 +1,53 @@
+define([
+    'base/js/namespace',
+    'base/js/events'
+    ], function(Jupyter, events) {
+
+        // Adds a cell above current cell (will be top if no cells)
+        var add_cell = function() {
+        Jupyter.notebook.
+        insert_cell_above('code').
+        // Define default cell here
+        set_text(`# Standard data science libraries
+import pandas as pd
+import numpy as np
+from scipy import stats
+import featuretools as ft
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('bmh')
+
+# Options for pandas
+pd.options.display.max_columns = 20
+
+# Display all cell outputs
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = 'all'
+`);
+Jupyter.notebook.select_prev;
+Jupyter.notebook.execute_cell_and_select_next;
+      };
+      // Button to add default cell
+      var defaultCellButton = function () {
+          Jupyter.toolbar.add_buttons_group([
+              Jupyter.keyboard_manager.actions.register ({
+                  'help': 'Add default cell',
+                  'icon' : 'fa-play-circle',
+                  'handler': add_cell
+              }, 'add-default-cell', 'Default cell')
+          ])
+      }
+    // Run on start
+    function load_ipython_extension() {
+        // Add a default cell if there are no cells
+        if (Jupyter.notebook.get_cells().length===1){
+            add_cell();
+        }
+        defaultCell_button();
+    }
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
